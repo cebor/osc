53b3117725e86978ae730c7fd7b191bfeebc6d1c
Felix <felix@stkn.org>
2014-08-10 20:49:52 +0200

    Add parallel auths & requests

--- osc.py
+++ osc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from gevent import monkey; monkey.patch_all()
+from gevent.pool import Pool
+from gevent.queue import Queue
 from pyquery import PyQuery as pq
 from urlparse import parse_qs
 import requests
@@ -19,12 +22,14 @@ if os.path.isfile('config.ini'):
   }
 
   ids = {
-    'kontoOnTop': config.get('IDS', 'kontoOnTop'),
+    'kontoOnTop': config.get('IDS', 'konto_on_top'),
     'konto': config.get('IDS', 'konto')
   }
 
-  startId = int(config.get('BRUTEFORCE', 'startId'))
+  startId = int(config.get('BRUTEFORCE', 'start_id'))
   rng = int(config.get('BRUTEFORCE', 'range'))
+  pa = int(config.get('BRUTEFORCE', 'parallel_auths'))
+  pr = int(config.get('BRUTEFORCE', 'parallel_requests'))
 
   url = 'https://qis.fh-rosenheim.de/qisserver/'
   auth_url = url + 'rds?state=user&type=1&category=auth.login'
@@ -34,28 +39,41 @@ if os.path.isfile('config.ini'):
       '|studiengang:stg=INF|kontoOnTop:labnrzu=%(kontoOnTop)s|konto:labnrzu=%(konto)s' % ids
 
 
-s = requests.session()
+queue = Queue(pa)
+pool = Pool(pr)
 
-def asi():
+
+def init_queue():
+  for r in pool.imap_unordered(lambda _: auth_asi(), range(pa)):
+    queue.put(r)
+
+
+def auth_asi():
+  s = requests.session()
   s.post(auth_url, data=auth)
   r = s.get(link_url)
   doc = pq(r.text)
-  asi, = parse_qs(doc('a[href*="&asi="]').attr('href'))['asi']
-  return '&asi=%s' % asi
+  asi = parse_qs(doc('a[href*="&asi="]').attr('href'))['asi'][0]
+  return {'session': s, 'asi': asi}
 
 
-def bruteforce(asi):
-  for i in range(startId, startId + rng):
-    param = '|pruefung:labnr=%s' % i
+def bruteforce(i):
+  auth = queue.get()
+  queue.put(auth)
 
-    r = s.get(content_url + param + asi)
-    doc = pq(r.text)
+  s = auth['session']
+  asi = auth['asi']
 
-    subject = doc('table tr td.tabelle1:nth-child(2)').text()
-    score = doc('table tr td.tabelle1:nth-child(4)').text()
+  param = '|pruefung:labnr=%s' % i
 
-    if subject and score:
-      print(subject + ' - ' + score)
+  r = s.get(content_url + param + '&asi=' + asi)
+  doc = pq(r.text)
+
+  subject = doc('table tr td.tabelle1:nth-child(2)').text()
+  score = doc('table tr td.tabelle1:nth-child(4)').text()
+
+  if subject and score:
+    print(subject + ' - ' + score)
 
 
 def check_config():
@@ -65,10 +83,12 @@ def check_config():
     config.add_section('BRUTEFORCE')
     config.set('CREDENTIALS', 'user', 'username')
     config.set('CREDENTIALS', 'pw', 'passsword')
-    config.set('IDS', 'kontoOnTop', '')
+    config.set('IDS', 'konto_on_top', '')
     config.set('IDS', 'konto', '')
-    config.set('BRUTEFORCE', 'startId', '1873537')
+    config.set('BRUTEFORCE', 'start_id', '1873537')
     config.set('BRUTEFORCE', 'range', '1000')
+    config.set('BRUTEFORCE', 'parallel_auths', '2')
+    config.set('BRUTEFORCE', 'parallel_requests', '2')
     with open('config.ini', 'w') as configfile:
       config.write(configfile)
     print('Creating config.ini! Please setup and rerun!')
@@ -78,4 +98,6 @@ def check_config():
 
 if __name__ == "__main__":
   if check_config():
-    bruteforce(asi())
+    init_queue()
+    for i in range(startId, startId + rng):
+      pool.spawn(bruteforce, i)
